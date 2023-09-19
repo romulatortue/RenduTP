8dacfd7aa875894561b65f96911ea87ec4aad696
Niss Romuald <romuald.niss@gmail.com>
2023-09-19 10:25:43 +0200

    Création du fichier

---
+++ tp_note.js
@@ -0,0 +1,8 @@
+"use strict";
+class Pokemon {
+    constructor(nom, pointsDeVie, pointsAttaque) {
+        this.nom = nom;
+        this.pointsDeVie = pointsDeVie;
+        this.pointsAttaque = pointsAttaque;
+    }
+}
---
+++ tp_note.ts
@@ -0,0 +1,12 @@
+class Pokemon {
+    private nom: string;
+    private pointsDeVie: number;
+    private pointsAttaque: number;
+
+
+    constructor(nom: string, pointsDeVie: number,pointsAttaque: number) {
+        this.nom=nom;
+        this.pointsDeVie=pointsDeVie;
+        this.pointsAttaque=pointsAttaque;
+    }
+}
