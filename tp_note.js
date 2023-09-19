5e2d77067c48b646b50e3dc7247722fc05ae9b81
Niss Romuald <romuald.niss@gmail.com>
2023-09-19 10:31:57 +0200

    Création des méthodes

--- tp_note.js
+++ tp_note.js
@@ -5,4 +5,17 @@ class Pokemon {
         this.pointsDeVie = pointsDeVie;
         this.pointsAttaque = pointsAttaque;
     }
+    estMort() {
+        if (this.pointsDeVie < 1) {
+            return true;
+        }
+        else
+            return false;
+    }
+    attaquer(p) {
+        p.pointsDeVie -= this.pointsAttaque;
+    }
+    afficherInformation() {
+        console.log("Votre pokémon " + this.nom + " à actuellement " + this.pointsDeVie + " il inflige " + this.pointsAttaque + " dégats par attaques");
+    }
 }
